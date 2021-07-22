12d8dfc051b6375224eb943093b9a747f25e3ad9
Charles Summers <charles.f.summers@rrd.com>
2021-07-21 21:19:51 -0500

    Add project files.

---
+++ SummersManorHalloweenPuzzle/ClientApp/src/components/column.jsx
@@ -0,0 +1,45 @@
+﻿import React from 'react';
+import styled from 'styled-components';
+import { Droppable } from 'react-beautiful-dnd';
+import Task from './task';
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  width: 220px;
+
+  display: flex;
+  flex-direction: column;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+  transition: background-color 0.2s ease;
+  background-color: ${props => (props.isDropDisabled ? 'red' : props.isDraggingOver ? 'skyblue' : 'white')};
+  flex-grow: 1;
+  min-height: 100px;
+`;
+
+export default class Column extends React.Component {
+    render() {
+        const isDropDisabled = this.props.column.id === 'column-3';
+        return (
+            <Container>
+                <Title>{this.props.column.title}</Title>
+                <Droppable droppableId={this.props.column.id} isDropDisabled={isDropDisabled}>
+                    {(provided, snapshot) => (
+                        <TaskList ref={provided.innerRef} {...provided.droppableProps} isDraggingOver={snapshot.isDraggingOver} isDropDisabled={isDropDisabled} >
+                            {this.props.tasks.map((task, index) => (
+                                <Task key={task.id} task={task} index={index} />
+                            ))}
+                            {provided.placeholder}
+                        </TaskList>
+                    )}
+                </Droppable>
+            </Container>
+        );
+    }
+}
---
+++ SummersManorHalloweenPuzzle/ClientApp/src/components/task.jsx
@@ -0,0 +1,32 @@
+﻿import React from 'react';
+import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: ${props => (props.isDragDisabled ? 'lightgrey' : props.isDragging ? 'lightgreen' : 'white')};
+`;
+
+export default class Task extends React.Component {
+    render() {
+        const isDragDisabled = this.props.task.id === 'task-5';
+        return (
+            <Draggable draggableId={this.props.task.id} index={this.props.index} isDragDisabled={isDragDisabled}>
+                {(provided, snapshot) => (
+                    <Container
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                        isDragging={snapshot.isDragging}
+                        isDragDisabled={isDragDisabled}
+                    >
+                        {this.props.task.content}
+                    </Container>
+                )}
+            </Draggable>
+        );
+    }
+}
