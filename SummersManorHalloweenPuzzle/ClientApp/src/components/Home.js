79e90426326d5581f409c07501ccb2c2fb47e639
Charles Summers <charles.f.summers@rrd.com>
2021-08-06 23:06:57 -0500

    Added Code for solving riddles

--- SummersManorHalloweenPuzzle/ClientApp/src/components/Home.js
+++ SummersManorHalloweenPuzzle/ClientApp/src/components/Home.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import Riddle from './Riddle';
 import riddleData from './riddle-data';
 
-export default function Home() {
-    const [riddle, setRiddle] = React.useState(riddleData.riddles["riddle1"]);
+var riddleKeys = Object.keys(riddleData.riddles);
+var startIndex = 1;//Math.floor(Math.random() * riddleKeys.length);
+
+export default function Home() {    
+    var currentIndex = startIndex;
+    const [riddle, setRiddle] = React.useState(riddleData.riddles[riddleKeys[startIndex]]);
+    function onSolved() {
+        currentIndex++;
+        if (currentIndex + 1 > riddleKeys.length) {
+            currentIndex = 0;
+        }
+        if (currentIndex === startIndex) {
+            //TODO Write Code to start final puzzle
+        } else {
+            setRiddle(riddleData.riddles[riddleKeys[currentIndex]]);
+        }
+    }
     return (
-        <Riddle RiddleData={riddle} />
+        <Riddle RiddleData={riddle} onSolved = {onSolved} />
     );
 }
--- SummersManorHalloweenPuzzle/ClientApp/src/components/Riddle.jsx
+++ SummersManorHalloweenPuzzle/ClientApp/src/components/Riddle.jsx
@@ -1,17 +1,63 @@
 import React from 'react';
 import ReactCodeInput from "react-code-input";
 import styled from 'styled-components';
+import ReactPinField from "react-pin-field"
 
 const Container = styled.div``;
 const RiddleText = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  display: flex;
+  flex-direction: column;
   color: white;
 `;
 
 export default function Riddle(props) {
+    var onSolved = props.onSolved;
+    var answer = props.RiddleData.answer;
+    function AddAudio(props) {
+        if (props.type === "audio") {
+            return (
+                ""//TODO ADD AUDIO CODE
+            )
+        } else {
+            return ("");
+        }
+    }
+
+    function AddClue(props) {
+        if (props.clueText !== "") {
+            return (
+                ""//TODO CLUE CODE
+            )
+        } else {
+            return ("");
+        }
+    }
+
+    function AddBonus(props) {
+        if (props.bonusText !== "") {
+            return (
+                ""//TODO CLUE CODE
+            )
+        } else {
+            return ("");
+        }
+    }
+
+    function TextChange(value) {
+        if (value.toLowerCase() === answer.toLowerCase()) {
+            onSolved();
+        }
+    }
     return (
         <Container>
             <RiddleText>{props.RiddleData.riddle}</RiddleText>
-            <ReactCodeInput type='text' fields={props.RiddleData.answer.length} />
+            <AddAudio type={props.RiddleData.type} />
+            <AddClue clueText={props.RiddleData.clueText} />
+            <AddBonus bonusText={props.RiddleData.bonusText} bonusAnswer={props.RiddleData.bonusAnswer} />
+            <ReactCodeInput type='text' fields={props.RiddleData.answer.length} onChange={TextChange} />
         </Container>        
     )
 }
