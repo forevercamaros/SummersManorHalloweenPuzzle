33c71fb4370a0d15ca660c924fc7a4f63dfa7a39
Charles Summers <charles.f.summers@rrd.com>
2021-08-09 14:24:08 -0500

    moving right along

--- SummersManorHalloweenPuzzle/ClientApp/src/components/Riddle.jsx
+++ SummersManorHalloweenPuzzle/ClientApp/src/components/Riddle.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactCodeInput from "react-code-input";
+import React, { useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import ReactAudioPlayer from 'react-audio-player'
 import soundFile from '../ChopinsFuneralMarch.mp3'
+import { Container, Row, Col } from 'react-grid-system';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 
-const Container = styled.div``;
+
 const RiddleText = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
@@ -13,38 +17,104 @@ const RiddleText = styled.div`
   flex-direction: column;
   color: white;
 `;
+const AudioHelpText = styled.div`
+  margin: 8px;
+  display: flex;
+  flex-direction: column;
+  color: white;
+`;
 
-export default function Riddle(props) {
-    var onSolved = props.onSolved;
-    var answer = props.RiddleData.answer;
-    function AddAudio(props) {
-        if (props.type === "audio") {
+export default function Riddle({ onSolved, RiddleData }) {
+    var onSolved = onSolved;
+    var answer = RiddleData.answer;
+    const answerElement = useRef(null);
+    var bonusText = Riddle.bonusText;
+
+    const [showClue, setShowClue] = useState(false);
+    const [clueRevealed, setClueRevealed] = useState(false);
+    const handleCloseClue = () => setShowClue(false);
+    const handleShowClue = () => {
+        setClueRevealed(true);
+        setShowClue(true);
+    };
+
+    const [showBonus, setShowBonus] = useState(false);
+    const handleCloseBonus = () => setShowBonus(false);
+    const handleShowBonus = () => setShowBonus(true);
+
+    useEffect(() => {
+        if (answerElement.current) {
+            answerElement.current.focus();
+        }
+    }, []);
+
+    function AddAudio({ type }) {
+        if (type === "audio") {
             return (
-                <ReactAudioPlayer
-                    src={soundFile}
-                    autoPlay
-                    controls
-                />
+                <>
+                    <Row>
+                        <Col>
+                            <AudioHelpText>If Audio doesn't play automatically. Click the play button</AudioHelpText>
+                        </Col>                    
+                </Row>
+                    <Row>
+                    <Col>
+                        <ReactAudioPlayer
+                            src={soundFile}
+                            autoPlay
+                            controls
+                        />
+                    </Col>                    
+                </Row>
+                </>         
             )
         } else {
             return ("");
         }
     }
 
-    function AddClue(props) {
-        if (props.clueText !== "") {
+    function AddClue({ clueText, clue }) {
+        if (clueText !== "" && typeof clueText !== 'undefined') {
             return (
-                ""//TODO ADD CLUE
+                <>
+                    <RiddleText>{clueText}</RiddleText>
+                    <Button variant="primary" onClick={handleShowClue}>
+                        Reveal Clue
+                    </Button>
+
+                    <Modal show={showClue} onHide={handleCloseClue}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Your Clue</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>{clue}</Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="primary" onClick={handleCloseClue}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </>
             )
         } else {
             return ("");
         }
     }
 
-    function AddBonus(props) {
-        if (props.bonusText !== "") {
+    function AddBonus({ bonusText }) {
+        if (bonusText !== "" && typeof bonusText !== 'undefined') {
             return (
-                ""//TODO BONUS CODE
+                <Modal show={showBonus} onHide={handleCloseBonus}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Your Bonus</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>{bonusText}</Modal.Body>
+                    <Modal.Footer>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text id="basic-addon1">Answer</InputGroup.Text>
+                            <FormControl ref={answerElement} onChange={TextChange} />
+                        </InputGroup>
+                    </Modal.Footer>
+                </Modal>
             )
         } else {
             return ("");
@@ -52,17 +122,35 @@ export default function Riddle(props) {
     }
 
     function TextChange(value) {
-        if (value.toLowerCase() === answer.toLowerCase()) {
-            onSolved();
+        if (value.currentTarget.value.toLowerCase() === answer.toLowerCase()) {
+            if (bonusText === "" || typeof bonusText === 'undefined' || clueRevealed === true) {
+                onSolved();
+            } else {
+                handleShowBonus();
+            }
+            
         }
-    }
+    }   
+
+
     return (
-        <Container>
-            <RiddleText>{props.RiddleData.riddle}</RiddleText>
-            <AddAudio type={props.RiddleData.type} />
-            <AddClue clueText={props.RiddleData.clueText} />
-            <AddBonus bonusText={props.RiddleData.bonusText} bonusAnswer={props.RiddleData.bonusAnswer} />
-            <ReactCodeInput type='text' fields={props.RiddleData.answer.length} onChange={TextChange} />
+        <Container fluid>
+            <Row>
+                <Col>
+                    <RiddleText>{RiddleData.riddle}</RiddleText>
+                </Col>
+            </Row>
+            <AddAudio type={RiddleData.type} />            
+            <AddClue clueText={RiddleData.clueText} clue={RiddleData.clue} />
+            <AddBonus bonusText={RiddleData.bonusText} bonusAnswer={RiddleData.bonusAnswer} />
+            <Row>
+                <Col>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon1">Answer</InputGroup.Text>
+                        <FormControl ref={answerElement} onChange={ TextChange }/>
+                    </InputGroup>
+                </Col>
+            </Row>            
         </Container>        
     )
 }
