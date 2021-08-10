abef7848a7e5ea8a1ed1be2174ac6687824839b1
Charles Summers <charles.f.summers@rrd.com>
2021-08-10 15:40:23 -0500

    added circle timer

--- SummersManorHalloweenPuzzle/ClientApp/src/components/Home.js
+++ SummersManorHalloweenPuzzle/ClientApp/src/components/Home.js
@@ -4,6 +4,7 @@ import riddleData from './riddle-data';
 import styled from 'styled-components';
 import FinalPuzzle from './FinalPuzzle';
 import { Transition } from 'react-transition-group';
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 var riddleKeys = Object.keys(riddleData.riddles);
 var startIndex = Math.floor(Math.random() * riddleKeys.length);
@@ -18,6 +19,30 @@ const FinalFadeContainer = styled.div`
   transition: opacity  ${props => props.duration}ms ease-in-out;
   opacity: ${props => (props.state === 'entering' || props.state === 'entered' ? '1' : '0')};
 `;
+
+function formatTimeString(seconds) {
+    var sec_num = parseInt(seconds, 10); // don't forget the second param
+    var hours = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours < 10) { hours = "0" + hours; }
+    if (minutes < 10) { minutes = "0" + minutes; }
+    if (seconds < 10) { seconds = "0" + seconds; }
+    return hours + ':' + minutes + ':' + seconds;
+}
+
+const renderTime = ({ remainingTime }) => {
+    if (remainingTime === 0) {
+        return <div className="timer">Too lale...</div>;
+    }
+
+    return (
+        <div>
+            {formatTimeString(remainingTime)}
+        </div>
+    );
+};
 
 export default function Home() {
     const fadeDuration = 1000;
@@ -45,6 +70,15 @@ export default function Home() {
     }
     return (
         <>
+            <CountdownCircleTimer
+                isPlaying
+                size={100}
+                duration={120}
+                colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+                onComplete={() => [true, 1000]}
+            >
+                {renderTime}
+            </CountdownCircleTimer>
             <Transition in={showRiddle} timeout={fadeDuration} onExited={nextRiddle} mountOnEnter={true} unmountOnExit={true}>
                 {state => (
                     <FadeContainer state={state} duration={fadeDuration}>
