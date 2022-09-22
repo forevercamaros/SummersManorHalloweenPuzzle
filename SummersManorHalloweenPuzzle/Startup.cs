7de716fb8e371daed661209599bbaf27168fdbf9
Charles Summers <Charles_Summers@hotmail.com>
2022-09-22 09:48:18 -0500

    Initial Changes for doecker

--- SummersManorHalloweenPuzzle/Startup.cs
+++ SummersManorHalloweenPuzzle/Startup.cs
@@ -44,7 +44,7 @@ namespace SummersManorHalloweenPuzzle
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
